f0d9d0f4f32fe2402bf9005db5ea7eb1269f3187
(no author) <(no author)@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2008-11-28 12:01:15 +0000

    renamed

--- slushr/server/models.py
+++ slushr/server/models.py
@@ -1,7 +1,16 @@
 from google.appengine.ext import db
 
-class Point(db.Model):
-  location_area_code = db.StringProperty()
+class Cell(db.Model):
+  date_created = db.DateTimeProperty(auto_now_add=True,auto_now=False)
   cell_id = db.StringProperty()
+
+class LocationArea(db.Model):
+  date_created = db.DateTimeProperty(auto_now_add=True,auto_now=False)
+  location_area_code = db.StringProperty()
+
+class Point(db.Model):
+  date_created = db.DateTimeProperty(auto_now_add=True,auto_now=False)
+  location_area = db.ReferenceProperty(LocationArea) # foreign key
+  cell = db.ReferenceProperty(Cell) # foreign key
   gps_location = db.GeoPtProperty()
-  label = db.StringProperty()
+
