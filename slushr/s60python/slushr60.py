ef75ee11368d219877ed87bc65773cad312f59b1
(no author) <(no author)@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2008-11-28 12:02:16 +0000

    moving

--- slushr/s60python/slushr60.py
+++ slushr/s60python/slushr60.py
@@ -20,7 +20,8 @@
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ## THE SOFTWARE.
 
-## Ossi locator daemon
+## slushd client for harvesting and matching cell information data to GPS
+## location info
 ##
 ## written for OtaSizzle / Ossi
 ## 27/11/2008
@@ -31,34 +32,47 @@ import positioning
 import location
 import urllib
 import httplib
+from socket import *
 
-# call back handler
+# callback handler
 def cb(event): 
   # getting GSM info
-  mobile_country_code, mobile_network_code, location_area_code, cell_id = location.gsm_location()
+  gsm_location = location.gsm_location()
+  if gsm_location:
+    mobile_country_code, mobile_network_code, location_area_code, cell_id = gsm_location
+  else:
+    print "ERROR in GSM location: "+str(gsm_location)
+    return False
   print "" 
   print "---" 
   print "GPS: "+str(event['position']['latitude'])+", "+str(event['position']['longitude'])
   print "GSM: "+str(cell_id)
-  print "sending to slushr..."
-
-
-  post_data = urllib.urlencode({  "location_area_code" : location_area_code,
-                        "cell_id" : cell_id,
-                        "latitude" : str(event['position']['latitude']),
-                        "longitude" : str(event['position']['longitude'])})
+  if str(event['position']['longitude']) != 'NaN' and str(event['position']['latitude']) != 'NaN':
+    print "sending to slushd server..."
+    post_data = urllib.urlencode({  "location_area_code" : location_area_code,
+                          "cell_id" : cell_id,
+                          "latitude" : str(event['position']['latitude']),
+                          "longitude" : str(event['position']['longitude'])})
                         
-  headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-  conn = httplib.HTTPConnection("slushr.appspot.com")
-  conn.request("POST", "/new", post_data, headers)
-  response = conn.getresponse()
-  data = response.read()
-  if response:
-      print "Data: "+str(data)
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    conn = httplib.HTTPConnection("slushd.appspot.com")
+    conn.request("POST", "/new", post_data, headers)
+    response = conn.getresponse()
+    data = response.read()
+    if response:
+        print "Data sent successfully!"
+    else:
+        print "Error sending data to slushd..."
+    conn.close()
   else:
-      print "Error sending data to slushr..."
-#  conn.close()
+    print "no valid GPS position"
 
+# get a fixed access point
+apid = select_access_point()  #Prompts you to select the access point
+apo = access_point(apid)      #apo is the access point you selected
+set_default_access_point(apo) #Sets apo as the default access point
+
+# start positioning
 positioning.set_requestors([{"type":"service", "format":"application", "data":"test_app"}]) 
 positioning.position(course=0, satellites=0, callback=cb, interval=5000000, partial=1)
 
