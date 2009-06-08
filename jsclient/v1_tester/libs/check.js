a0cdd64b337c4c083fd06c45b6cbe129fd6e15ba
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-06-08 21:01:42 +0000

    Modified logger to use get and impemented it on check.js

--- jsclient/v1_tester/libs/check.js
+++ jsclient/v1_tester/libs/check.js
@@ -1,12 +1,12 @@
 function assertEquals( message, param1, param2 ) {
 	if( param1 != param2 ) {
 		// do a post to error.log
-		alert(message);
+		http_request( 'GET' , './log.php?error=' + message, null);
 	}
 }
 
 function assertNotNull( message, param1 ) {
 	if( param1 == null )  {
-		alert(message);
+		http_request( 'GET' , './log.php?error=' + message, null);;
 	}
 }
--- jsclient/v1_tester/log.php
+++ jsclient/v1_tester/log.php
@@ -1,8 +1,8 @@
 <?php
 	$browser = $_SERVER['HTTP_USER_AGENT'];
 	$error = '';
-	if( isset( $_POST['error'] ) ) {
-		$error = $_POST['error'];
+	if( isset( $_GET['error'] ) ) {
+		$error = $_GET['error'];
 	}
 	$res = fopen( 'log.file' , 'a+' );
 	$string = $browser.' '.$error.'\n';
