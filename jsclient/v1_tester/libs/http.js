921092251d78fe20c1cefacbad09a4057b859845
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-06-09 08:45:19 +0000

    Work with the http-library and logger

--- jsclient/v1_tester/libs/http.js
+++ jsclient/v1_tester/libs/http.js
@@ -1,23 +1,26 @@
 // Common HTTP request components
 
-function http_request( method, url, parameters ){
+function http_request( method, url, parameters, f ){
         // this should be a smart selector...
         http = new XMLHttpRequest();
         // parameters need to be encoded
-        http.open( method , url, false);
+        http.open( method , url, true);
+	http.onreadystatechange = function() { 
+		if( http.readyState == 4 ) {
+			response = '';
+			if( http.responseText != '' ) {
+               			 response = eval( '(' + http.responseText + ')' );
+        		}
+			f();
+		}
+	};
         if( parameters != null ) {
                 http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 http.setRequestHeader("Content-length", parameters.length);
                 http.setRequestHeader("Connection", "close");
                 http.send( http_parameter( parameters ) );
-        } else {
+        } else {
                http.send( null );
-        }
-        while( http.readyState != 4 ) {
-                // this is syncronous method
-        }
-        if( http.responseText != '' ) {
-                return eval( '(' + http.responseText + ')' );
         }
 }
 
@@ -30,3 +33,10 @@ function http_parameter( object ) {
         return ret;
 }
 
+function sleep( ms ) {
+	time = new Date();
+	nowTime = new Date();
+	while( nowTime - time < ms ) {
+		nowTime = new Date();
+	}
+}
