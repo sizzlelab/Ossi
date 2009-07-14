20c0960be5bff9d92a4ad180f72c1a0eb3ae8fb5
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-07-14 13:13:53 +0000

    Initial work on iframed floating ossi.

---
+++ jsclient/v1/channel/ossi_float.js
@@ -0,0 +1,85 @@
+ossiFloater = {
+
+  init: function(channelID, groupID){
+  
+    var WIDGET_VIEWPORT = {
+      height: 428,
+      width: 313
+    };
+    // Initial container div
+    var div = document.createElement('div');
+    div.style.display = 'none';
+    div.style.position = 'absolute';
+    div.style.top = '100px';
+    div.style.left = '100px';
+    div.style.background = '#000';
+    div.style.height = WIDGET_VIEWPORT.height;
+    div.style.width = WIDGET_VIEWPORT.width;
+    div.setAttribute('id', 'ossi-float');
+    document.body.appendChild(div);
+    // Show/hide button
+    var closeButton = document.createElement('div');
+    closeButton.innerHTML = 'O<br/>S<br/>S<br/>I<br/>';
+    closeButton.setAttribute('id', 'ossi-float-button');
+    var closeImage = document.createElement('img');
+    closeImage.setAttribute('src', '../images/ossi_minimize_button.png');
+    closeButton.appendChild(closeImage);
+    closeButton.style.position = 'absolute';
+    closeButton.style.right = '-29px';
+    closeButton.style.top = '10px';
+    closeButton.style.width = '25px';
+    closeButton.style.height = '90px';
+    closeButton.style.background = '#555';
+    closeButton.style.color = '#ff8a21';
+    closeButton.style.fontFamily = 'helvetica';
+    closeButton.style.fontWeight = 'bold';
+    closeButton.style.fontSize = '14px';
+    closeButton.style.textAlign = 'center';
+    closeButton.style.paddingTop = '10px';
+    closeButton.style.border = 'solid #444 2px';
+    var clickCount = 0;
+    closeButton.onclick = function(){
+      clickCount++;
+      if (clickCount % 2 == 0) {
+        ossi.style.display = 'inline';
+        closeImage.setAttribute('src', '../images/ossi_minimize_button.png');
+      }
+      else {
+        ossi.style.display = 'none';
+        closeImage.setAttribute('src', '../images/ossi_maximize_button.png')
+        div.style.left = 0;
+      }
+    };
+    div.appendChild(closeButton);
+    // Ossi iframe
+    var ossi = document.createElement('iframe');
+    ossi.setAttribute('src', 'http://ossi.alpha.sizl.org');
+    ossi.setAttribute('height', WIDGET_VIEWPORT.height);
+    ossi.setAttribute('width', WIDGET_VIEWPORT.width);
+    ossi.setAttribute('frameborder', 0);
+    div.appendChild(ossi);
+    
+    var tragMode = false;
+    document.onmousedown = function(event){
+      if (event.target.id == 'ossi-float-button') {
+        tragMode = true;
+      }
+    };
+    
+    document.onmousemove = function(event){
+      if (tragMode) {
+        console.log('A ' + event.clientX + ' ' + event.clientY);
+        kissa = event;
+        div.style.top = (event.clientY + 5) + 'px';
+        div.style.left = (event.clientX + 5) + 'px';
+      }
+    }
+    
+    document.onmouseup = function(event){
+      tragMode = false;
+    }
+    
+    // Loading done, show screen!
+    div.style.display = 'block';
+  }
+}
