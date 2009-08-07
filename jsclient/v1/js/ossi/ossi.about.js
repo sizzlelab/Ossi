4603b06e0d85e65976caacbc2986f70f0f8bc324
turunen <turunen@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-08-07 06:53:12 +0000

    corrected wrong support address

--- jsclient/v1/js/ossi/ossi.about.js
+++ jsclient/v1/js/ossi/ossi.about.js
@@ -26,7 +26,7 @@ ossi.about = Class.create(ossi.base,{
           				<div id="about_Ossi" style="margin:20px 10px 20px 10px">\
           				  <h2>About Ossi</h2>\
                     <p>Ossi is a browser-based social networking service enabling media rich communication between users on mobile and web platforms. Ossi is a part of the OtaSizzle research project that develops a mobile living lab in the Otaniemi campus. Students actively participate in the developing of both Ossi and of the OtaSizzle platform. OtaSizzle as well as Ossi are in beta phase i.e. under development. This might result in some technical problems every now and then. We count on your understanding and patience and are grateful for all feedback that you are willing to give us.</p>\
-                    <p>Please report all problems to <a href="mailto:helpdesk-otasizzle@hiit.fi">helpdesk-otasizzle@hiit.fi</a>.</p>\
+                    <p>Please report all problems to <a href="mailto:otasizzle-helpdesk@hiit.fi">otasizzle-helpdesk@hiit.fi</a>.</p>\
                   </div>\
           				<div class="nav_button">\
           					<a id="back_button" class="nav_button_text" href="javascript:void(null);">Back</a>\
