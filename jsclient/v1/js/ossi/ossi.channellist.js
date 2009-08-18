7d8ddcf959421bb31a09c1425cb3d9d244b12e84
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-08-18 09:53:20 +0000

    Removed debug

--- jsclient/v1/js/ossi/ossi.channellist.js
+++ jsclient/v1/js/ossi/ossi.channellist.js
@@ -1,110 +1,132 @@
 /**
-* ossi channellist class
-*/
-ossi.channellist = Class.create(ossi.base,{
-	initialize: function(parent,options) {
+ * ossi channellist class
+ */
+ossi.channellist = Class.create(ossi.base, {
+  initialize: function(parent, options){
     this.parent = parent;
-		this.options = Object.extend({
-      groupId : false,
-      selfUpdate : false,
-      hostElement : false
-	  },options);
+    this.options = Object.extend({
+      groupId: false,
+      selfUpdate: false,
+      hostElement: false
+    }, options);
     this.updateInterval = 20000;
     this.updateOptions = {
-      per_page : 8,
-      page : 1
+      per_page: 8,
+      page: 1
     };
-	  this.pane = false;
+    this.pane = false;
     this._draw();
-    if (this.options.groupId != false) $('create_channel_button_container').hide();
+    if (this.options.groupId != false) 
+      $('create_channel_button_container').hide();
     this._resetInterval(); // this resets the intervalled update call, if selfUpdate is enabled
-	},
-	/**
-	* _update
-	*
-	* does not handle XHR failure yet!
-	*/
-	update: function(options) {
-    if (typeof(this.parent.userId) == 'undefined') return; // userId in the parent controller not set
+  },
+  /**
+   * _update
+   *
+   * does not handle XHR failure yet!
+   */
+  update: function(options){
+    if (typeof(this.parent.userId) == 'undefined') 
+      return; // userId in the parent controller not set
     var self = this;
-
+    
     // get channels
-    var URL = BASE_URL+'/channels';
-    var params = { per_page : this.updateOptions.per_page, page : this.updateOptions.page };
-    if (self.options.groupId != false) params.group_id = self.options.groupId;
+    var URL = BASE_URL + '/channels';
+    var params = {
+      per_page: this.updateOptions.per_page,
+      page: this.updateOptions.page
+    };
+    if (self.options.groupId != false) 
+      params.group_id = self.options.groupId;
     self.parent.showLoading();
-    new Ajax.Request(URL,{
-      method : 'get',
-      parameters : params,
-      requestHeaders : (client.is_widget) ? ['Cookie',self.parent.sessionCookie] : '',
-      onSuccess : function(response) {
+    new Ajax.Request(URL, {
+      method: 'get',
+      parameters: params,
+      requestHeaders: (client.is_widget) ? ['Cookie', self.parent.sessionCookie] : '',
+      onSuccess: function(response){
         var json = response.responseJSON;
         if (typeof(json.entry) != 'undefined') {
           if (json.entry.length > 0) {
             self._drawContents(json.entry);
-    			  $('channels_next_button_container').setStyle({ 'width': '100%' });
-            if (self.updateOptions.per_page*self.updateOptions.page >= json.pagination.size) { // on the last page or less than one page of results
-              console.log('1');
+            $('channels_next_button_container').setStyle({
+              'width': '100%'
+            });
+            if (self.updateOptions.per_page * self.updateOptions.page >= json.pagination.size) { // on the last page or less than one page of results
               if (json.pagination.size > self.updateOptions.per_page) { // last page
                 $('channels_back_button_2_container').show();
-        			  $('channels_previous_button_container').setStyle({ 'width': '100%' });
-        			  $('channels_previous_button_container').show();
-        			  $('channels_next_button_container').hide();
-              } else { // first page and not enough results for pagination
-        			  $('channels_previous_button_container').hide();
-        			  $('channels_next_button_container').hide();
+                $('channels_previous_button_container').setStyle({
+                  'width': '100%'
+                });
+                $('channels_previous_button_container').show();
+                $('channels_next_button_container').hide();
               }
-            } else if (self.updateOptions.page > 1) { // between first and last page
-              console.log('2');
-              $('channels_back_button_2_container').show();
-      			  $('channels_previous_button_container').setStyle({ 'width': '50%' });
-      			  $('channels_next_button_container').setStyle({ 'width': '50%' });
-      			  $('channels_next_button_container').show();
-      			  $('channels_previous_button_container').show();
-            } else { // first page
-              console.log('3');
-              $('channels_back_button_2_container').show();
-      			  $('channels_previous_button_container').setStyle({ 'width': '0%' });
-    			    $('channels_next_button_container').show();
-    			    $('channels_next_button_container').setStyle({ 'width': '100%' });
-      			  $('channels_previous_button_container').hide();
+              else { // first page and not enough results for pagination
+                $('channels_previous_button_container').hide();
+                $('channels_next_button_container').hide();
+              }
             }
-          } else {
+            else 
+              if (self.updateOptions.page > 1) { // between first and last page
+                $('channels_back_button_2_container').show();
+                $('channels_previous_button_container').setStyle({
+                  'width': '50%'
+                });
+                $('channels_next_button_container').setStyle({
+                  'width': '50%'
+                });
+                $('channels_next_button_container').show();
+                $('channels_previous_button_container').show();
+              }
+              else { // first page
+                $('channels_back_button_2_container').show();
+                $('channels_previous_button_container').setStyle({
+                  'width': '0%'
+                });
+                $('channels_next_button_container').show();
+                $('channels_next_button_container').setStyle({
+                  'width': '100%'
+                });
+                $('channels_previous_button_container').hide();
+              }
+          }
+          else {
             $('channels_placeholder').replace('<div style="padding:10px; text-align:center">There are currently no channels available to you in the service. Please contact system administrators at: otasizzle-helpdesk@hiit.fi</div>');
           }
-        } else {
+        }
+        else {
           $('channels_placeholder').replace('<div style="padding:10px; text-align:center">Error occurred. Try again later.</div>');
           $('create_channel_button_container').hide();
         }
-        setTimeout(function() {
+        setTimeout(function(){
           self.parent.hideLoading();
         }, 600);
       },
-      onFailure : function(response) {
+      onFailure: function(response){
         alert('could not get channel list!!!');
       }
     });
-	},
-	_drawContents: function(entries) {
+  },
+  _drawContents: function(entries){
     var self = this;
     var h = '';
-    entries.each(function(entry) {
+    entries.each(function(entry){
       h += self._getButtonHTML(entry);
-    },self);
+    }, self);
     $('channels_placeholder').update(h);
     this._addLinkListeners();
   },
-  _draw: function() {
+  _draw: function(){
     if (this.options.hostElement) {
       this.options.hostElement.insert(this._getHTML());
       this._addListeners();
       this.pane = $('channellistpane');
-    } else {
+    }
+    else {
       alert('ossi.channellist._draw() failed! this.options.hostElement not defined!');
     }
   },
-  _getHTML: function() {
-    var h =   '\
+  _getHTML: function(){
+    var h = '\
           			<div id="channellistpane" style="display:none; position:absolute; top:0px; left:0px; width:100%">\
           				<div id="channels_back_button_2_container" class="nav_button" style="display:none">\
           					<a id="channels_back_button2" class="nav_button_text" href="javascript:void(null);">Back</a>\
@@ -133,77 +155,99 @@ ossi.channellist = Class.create(ossi.base,{
           		';
     return h;
   },
-  _getButtonHTML: function(channel) {
+  _getButtonHTML: function(channel){
     var updated_text = '';
     if (channel.updated_at != 'undefined') {
       if (channel.updated_at != null) {
-        updated_text = this.parent.utils.agoString( channel.updated_at );
+        updated_text = this.parent.utils.agoString(channel.updated_at);
       }
     }
     
     var creator_html = '';
     if (channel.metadata != null) {
       if (typeof(channel.metadata.creator) != 'undefined') {
-        creator_html = '<div class="button_subtitle_text" style="padding-top:2px">Originally created by '+channel.metadata.creator+'</div>';
+        creator_html = '<div class="button_subtitle_text" style="padding-top:2px">Originally created by ' + channel.metadata.creator + '</div>';
       }
     }
-
+    
     var ident_class = '';
-    if (! Object.isUndefined(channel.channel_type)) {
+    if (!Object.isUndefined(channel.channel_type)) {
       ident_class = (channel.channel_type == 'public') ? 'public' : 'private';
     }
     
-    var h =   '\
-          				<div class="channel_button" id="channel_id_'+channel.id+'">\
-                    <div class="channel_button_left_column '+ident_class+'"></div>\
+    var h = '\
+          				<div class="channel_button" id="channel_id_' + channel.id + '">\
+                    <div class="channel_button_left_column ' +
+    ident_class +
+    '"></div>\
                     <div class="channel_button_text">\
-        						  <div class="button_title" style="font-size:11px; font-weight:bold;"><a href="javascript:void(null);">'+channel.name+'</a></div>\
-        						  <div class="button_subtitle_text" style="padding:3px 0px 0px 15px">'+channel.description+'</div>\
-        						  <div class="button_subtitle_text" style="padding:0px 0px 0px 15px">Created by '+channel.owner_name+'</div>\
-        						  <div class="button_subtitle_text" style="padding:0px 0px 0px 15px"><!--'+channel.totalResults+' messages.--> Updated '+updated_text+'</div>\
+        						  <div class="button_title" style="font-size:11px; font-weight:bold;"><a href="javascript:void(null);">' +
+    channel.name +
+    '</a></div>\
+        						  <div class="button_subtitle_text" style="padding:3px 0px 0px 15px">' +
+    channel.description +
+    '</div>\
+        						  <div class="button_subtitle_text" style="padding:0px 0px 0px 15px">Created by ' +
+    channel.owner_name +
+    '</div>\
+        						  <div class="button_subtitle_text" style="padding:0px 0px 0px 15px"><!--' +
+    channel.totalResults +
+    ' messages.--> Updated ' +
+    updated_text +
+    '</div>\
                     </div>\
           				</div>\
           			';
     return h;
   },
-  _backHandler: function() {
+  _backHandler: function(){
     this.options.backCase.apply();
   },
-  _createChannelHandler: function() {
+  _createChannelHandler: function(){
     var self = this;
     self.parent.case19({
-      backCase : self.parent.case18.bind(self.parent,{
-        out : true,
-        backCase : self.parent.case3.bind(self.parent,{out:true}) 
+      backCase: self.parent.case18.bind(self.parent, {
+        out: true,
+        backCase: self.parent.case3.bind(self.parent, {
+          out: true
+        })
       })
     });
   },
-  _openChannelHandler: function(event,button_id) {
+  _openChannelHandler: function(event, button_id){
     var self = this;
-    var channel_id = button_id.replace("channel_id_","");
+    var channel_id = button_id.replace("channel_id_", "");
     self.parent.case20({
-      channelId : channel_id,
-      groupId : self.options.groupId,
-      backCase : self.parent.case18.bind(self.parent,{
-        groupId : self.options.groupId,
-        out : true,
-        backCase : self.parent.case3.bind(self.parent,{out:true}) 
+      channelId: channel_id,
+      groupId: self.options.groupId,
+      backCase: self.parent.case18.bind(self.parent, {
+        groupId: self.options.groupId,
+        out: true,
+        backCase: self.parent.case3.bind(self.parent, {
+          out: true
+        })
       })
     });
   },
-  _nextHandler: function() {
-    this.updateOptions = { page : ++this.updateOptions.page, per_page : 8 };
+  _nextHandler: function(){
+    this.updateOptions = {
+      page: ++this.updateOptions.page,
+      per_page: 8
+    };
     this.update();
     this._resetInterval(); // reset the interval as we just updated
     this.startIndex += this.count;
   },
-  _previousHandler: function() {
-    this.updateOptions = { page : --this.updateOptions.page, per_page : 8  };
+  _previousHandler: function(){
+    this.updateOptions = {
+      page: --this.updateOptions.page,
+      per_page: 8
+    };
     this.update();
     this._resetInterval(); // reset the interval as we just updated
     this.startIndex -= this.count;
   },
-  _aboutChannelsHandler: function() {
+  _aboutChannelsHandler: function(){
     var m = '\
                 <div style="font-size:10px; padding:10px">\
         				  <h2>About Channels</h2>\
@@ -212,15 +256,17 @@ ossi.channellist = Class.create(ossi.base,{
                 </div>\
     ';
     this.parent.case6({
-      message : m,
-      buttonText : "Back",
-      backCase:this.parent.case18.bind(this.parent,{
-        out:true,
-        backCase:this.parent.case3.bind(this.parent,{out:true})
+      message: m,
+      buttonText: "Back",
+      backCase: this.parent.case18.bind(this.parent, {
+        out: true,
+        backCase: this.parent.case3.bind(this.parent, {
+          out: true
+        })
       })
-    });            
+    });
   },
-  _addListeners: function() {
+  _addListeners: function(){
     $('create_channel_button').onclick = this._createChannelHandler.bindAsEventListener(this);
     $('about_channels_button').onclick = this._aboutChannelsHandler.bindAsEventListener(this);
     $('channels_back_button').onclick = this._backHandler.bindAsEventListener(this);
@@ -228,31 +274,45 @@ ossi.channellist = Class.create(ossi.base,{
     $('channels_previous_button').onclick = this._previousHandler.bindAsEventListener(this);
     $('channels_back_button2').onclick = this._backHandler.bindAsEventListener(this);
   },
-  _removeListeners: function() {
-    $('create_channel_button').onclick = function() { return }
-    $('about_channels_button').onclick = function() { return };
-    $('channels_back_button').onclick = function() { return }
-    $('channels_next_button').onclick = function() { return }
-    $('channels_previous_button').onclick = function() { return }
-    $('channels_back_button2').onclick = function() { return }
+  _removeListeners: function(){
+    $('create_channel_button').onclick = function(){
+      return
+    }
+    $('about_channels_button').onclick = function(){
+      return
+    };
+    $('channels_back_button').onclick = function(){
+      return
+    }
+    $('channels_next_button').onclick = function(){
+      return
+    }
+    $('channels_previous_button').onclick = function(){
+      return
+    }
+    $('channels_back_button2').onclick = function(){
+      return
+    }
   },
-  _addLinkListeners: function() { // for dynamic buttons
-    $$('.channel_button').each(function(button) {
-      button.onclick = this._openChannelHandler.bindAsEventListener(this,button.id);
-    },this);
+  _addLinkListeners: function(){ // for dynamic buttons
+    $$('.channel_button').each(function(button){
+      button.onclick = this._openChannelHandler.bindAsEventListener(this, button.id);
+    }, this);
   },
-  _removeLinkListeners: function() {
-    $$('.channel_button').each(function(button) {
-      button.onclick = function() { return };
-    },this);
+  _removeLinkListeners: function(){
+    $$('.channel_button').each(function(button){
+      button.onclick = function(){
+        return
+      };
+    }, this);
   },
-  _resetInterval: function() {
+  _resetInterval: function(){
     if (this.options.selfUpdate) {
       clearInterval(this.interval);
       this.interval = setInterval(this.update.bind(this), this.updateInterval);
     }
   },
-  destroy: function () {
+  destroy: function(){
     if (this.options.selfUpdate) {
       clearInterval(this.interval);
     }
