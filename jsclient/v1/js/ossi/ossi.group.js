94691f95e6c18373e793f0f3ae37f98fee64fa3f
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-08-18 09:25:08 +0000

    Handled special backcase for leaving group.

--- jsclient/v1/js/ossi/ossi.group.js
+++ jsclient/v1/js/ossi/ossi.group.js
@@ -1,85 +1,94 @@
 /**
-* ossi group class
-*/
-ossi.group = Class.create(ossi.base,{
-	initialize: function(parent,options) {
+ * ossi group class
+ */
+ossi.group = Class.create(ossi.base, {
+  initialize: function(parent, options){
     this.parent = parent;
-		this.options = Object.extend({
-      groupId : false,
-      hostElement : false
-	  },options);
-	  this.pane = false;
+    this.options = Object.extend({
+      groupId: false,
+      hostElement: false
+    }, options);
+    this.pane = false;
     this._draw();
-	},
-	/**
-	* _update
-	*
-	* does not handle XHR failure yet!
-	*/
-	update: function() {
+  },
+  /**
+   * _update
+   *
+   * does not handle XHR failure yet!
+   */
+  update: function(){
     var self = this;
-    var URL = BASE_URL+'/groups/@public/'+this.options.groupId;
+    var URL = BASE_URL + '/groups/@public/' + this.options.groupId;
     self.parent.showLoading();
     new Ajax.Request(URL, {
-      method : 'get',
-      requestHeaders : (client.is_widget) ? ['Cookie',self.parent.sessionCookie] : '',
-      onSuccess : function(response) { // does not handle invalid responses
+      method: 'get',
+      requestHeaders: (client.is_widget) ? ['Cookie', self.parent.sessionCookie] : '',
+      onSuccess: function(response){ // does not handle invalid responses
         var json = response.responseJSON;
-				json = json.entry;
-		
+        json = json.entry;
+        
         // title
-        if (! Object.isUndefined(json.title)) $('view_group_title').update(json.title);
-        else $('view_group_title').update('N/A');
-
+        if (!Object.isUndefined(json.title)) 
+          $('view_group_title').update(json.title);
+        else 
+          $('view_group_title').update('N/A');
+        
         // description
-        if (! Object.isUndefined(json.description)) $('view_group_description').update(json.description);
-        else $('view_group_description').update('N/A');
-
+        if (!Object.isUndefined(json.description)) 
+          $('view_group_description').update(json.description);
+        else 
+          $('view_group_description').update('N/A');
+        
         // type
-        if (! Object.isUndefined(json.group_type)) $('view_group_type').update(json.group_type + ' group');
-        else $('view_group_type').update('N/A');
-		
-    		if( json.is_member ) {
-    		  $('leave_button_container').show();
-  				$('join_button_container').hide();
-    		} else {
-    		  if (json.group_type != 'closed') $('join_button_container').show();
-  				$('leave_button_container').hide();
-    		}
-
+        if (!Object.isUndefined(json.group_type)) 
+          $('view_group_type').update(json.group_type + ' group');
+        else 
+          $('view_group_type').update('N/A');
+        
+        if (json.is_member) {
+          $('leave_button_container').show();
+          $('join_button_container').hide();
+        }
+        else {
+          if (json.group_type != 'closed') 
+            $('join_button_container').show();
+          $('leave_button_container').hide();
+        }
+        
         // now check if group has channels and display channels button accordingly
-        URL = BASE_URL+'/channels?group_id='+self.options.groupId;
+        URL = BASE_URL + '/channels?group_id=' + self.options.groupId;
         new Ajax.Request(URL, {
-          method : 'get',
-          requestHeaders : (client.is_widget) ? ['Cookie',self.parent.sessionCookie] : '',
-          onSuccess : function(response) { // does not handle invalid responses
+          method: 'get',
+          requestHeaders: (client.is_widget) ? ['Cookie', self.parent.sessionCookie] : '',
+          onSuccess: function(response){ // does not handle invalid responses
             var json = response.responseJSON;
             if (json.entry.length > 0) {
               $('group_channels_button_container').show();
             }
           }
         });
-
-        setTimeout(function() {
+        
+        setTimeout(function(){
           self.parent.hideLoading();
         }, 600);
       },
-      onFailure : function() {
+      onFailure: function(){
         alert('could not load group data!');
       }
     });
-	},
-  _draw: function() {
+  },
+  _draw: function(){
     if (this.options.hostElement) {
       this.options.hostElement.insert(this._getHTML());
       this._addListeners();
       this.pane = $('grouppane');
-    } else {
+    }
+    else {
       alert('ossi.group._draw() failed! this.options.hostElement not defined!');
     }
   },
-  _getHTML: function() {
-    var h =   '\
+  _getHTML: function(){
+    var h = '\
           			<div id="grouppane" style="display:none; position:absolute; top:0px; left:0px; width:100%">\
                   <form>\
                     <div style="margin: 12px auto 12px auto; text-align: left; width: 205px;">\
@@ -110,112 +119,106 @@ ossi.group = Class.create(ossi.base,{
           		';
     return h;
   },
-  _joinHandler: function() {
+  _joinHandler: function(){
     var self = this;
-    if (typeof(this.parent.userId) == 'undefined') return; // userId in the parent controller not set
-    var URL = BASE_URL+'/people/'+this.parent.userId+'/@groups';
-    var params = { group_id : this.options.groupId };
+    if (typeof(this.parent.userId) == 'undefined') 
+      return; // userId in the parent controller not set
+    var URL = BASE_URL + '/people/' + this.parent.userId + '/@groups';
+    var params = {
+      group_id: this.options.groupId
+    };
     self.parent.showLoading();
     new Ajax.Request(URL, {
-      method : 'post',
-      parameters : params,
-      requestHeaders : (client.is_widget) ? ['Cookie',self.parent.sessionCookie] : '',
-      onSuccess : function(response) { // does not handle invalid responses
+      method: 'post',
+      parameters: params,
+      requestHeaders: (client.is_widget) ? ['Cookie', self.parent.sessionCookie] : '',
+      onSuccess: function(response){ // does not handle invalid responses
         var json = response.responseJSON;
         self.parent.case6({
-          message : "You have successfully joined this group!",
-          buttonText : "Back",
-          backCase:self.parent.case27.bind(self.parent,{
-            groupId : self.options.groupId,
-            out:true,
-            backCase:self.parent.case25.bind(self.parent,{
-              out:true,
-              backCase:self.parent.case3.bind(self.parent,{out:true})
-            })
-          })
+          message: "You have successfully joined this group!",
+          buttonText: "Back"
         });
         self.parent.hideLoading();
       },
-      onFailure : function() {
+      onFailure: function(){
         alert('could not add user to group!');
       }
     });
   },
-  _leaveHandler: function() {
+  _leaveHandler: function(){
     var self = this;
-    if (typeof(this.parent.userId) == 'undefined') return; // userId in the parent controller not set
-    var URL = BASE_URL+'/people/'+this.parent.userId+'/@groups/' + this.options.groupId;
+    if (typeof(this.parent.userId) == 'undefined') 
+      return; // userId in the parent controller not set
+    var URL = BASE_URL + '/people/@me/@groups/' + this.options.groupId;
     self.parent.showLoading();
     new Ajax.Request(URL, {
-      method : 'delete',
-      requestHeaders : (client.is_widget) ? ['Cookie',self.parent.sessionCookie] : '',
-      onSuccess : function(response) { // does not handle invalid responses
+      method: 'delete',
+      requestHeaders: (client.is_widget) ? ['Cookie', self.parent.sessionCookie] : '',
+      onSuccess: function(response){ // does not handle invalid responses
         var json = response.responseJSON;
+        // remove one item from the back-stack
+        // (if user leaves the group, go back to group list)¨
+        // also, handles the case that the group is removed (only one member left)
+        self.parent.stack.pop();
         self.parent.case6({
-          message : "You have successfully left this group!",
-          buttonText : "Back",
-          backCase:self.parent.case25.bind(self.parent,{
-            out:true,
-            backCase:self.parent.case3.bind(self.parent,{out:true})
-          })
+          message: "You have successfully left this group!",
+          buttonText: "Back"
         });
         self.parent.hideLoading();
       },
-      onFailure : function() {
+      onFailure: function(){
         alert('could not add user to group!');
       }
     });
   },
-  _backHandler: function() {
+  _backHandler: function(){
     this.options.backCase.apply();
   },
-  _membersHandler: function() {
+  _membersHandler: function(){
     var self = this;
     self.parent.case28({
-      groupId : self.options.groupId,
-      backCase : self.parent.case27.bind(self.parent,{
-        out:true,
-        groupId:self.options.groupId,
-        backCase : self.parent.case25.bind(self.parent,{
-          out:true,
-          backCase : self.parent.case3.bind(self.parent,{
-            out:true
-          })
-        })
+      groupId: self.options.groupId,
+      backCase: self.parent.case27.bind(self.parent, {
+        out: true,
+        groupId: self.options.groupId
       })
     });
   },
-  _channelsHandler: function() {
+  _channelsHandler: function(){
     var self = this;
     self.parent.case18({
-      groupId : self.options.groupId,
-      backCase : self.parent.case27.bind(self.parent,{
-        out:true,
-        groupId:self.options.groupId,
-        backCase : self.parent.case25.bind(self.parent,{
-          out:true,
-          backCase : self.parent.case3.bind(self.parent,{
-            out:true
-          })
-        })
+      groupId: self.options.groupId,
+      backCase: self.parent.case27.bind(self.parent, {
+        out: true,
+        groupId: self.options.groupId
       })
     });
   },
-  _addListeners: function() {
+  _addListeners: function(){
     $('group_channels_button').onclick = this._channelsHandler.bindAsEventListener(this);
     $('members_button').onclick = this._membersHandler.bindAsEventListener(this);
     $('join_button').onclick = this._joinHandler.bindAsEventListener(this);
     $('leave_button').onclick = this._leaveHandler.bindAsEventListener(this);
     $('back_button').onclick = this._backHandler.bindAsEventListener(this);
   },
-  _removeListeners: function() {
-    $('group_channels_button').onclick = function() { return }
-    $('members_button').onclick = function() { return }
-    $('join_button').onclick = function() { return }
-    $('leave_button').onclick = function() { return }
-    $('back_button').onclick = function() { return }
+  _removeListeners: function(){
+    $('group_channels_button').onclick = function(){
+      return
+    }
+    $('members_button').onclick = function(){
+      return
+    }
+    $('join_button').onclick = function(){
+      return
+    }
+    $('leave_button').onclick = function(){
+      return
+    }
+    $('back_button').onclick = function(){
+      return
+    }
   },
-  destroy: function () {
+  destroy: function(){
     this._removeListeners();
     this.pane.remove();
   }
