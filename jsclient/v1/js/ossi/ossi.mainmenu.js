111ee359afa58735d6b12f26331796d43761c7f8
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-08-19 09:29:52 +0000

    Removed call to /@self/@location, part of @self nowdays

--- jsclient/v1/js/ossi/ossi.mainmenu.js
+++ jsclient/v1/js/ossi/ossi.mainmenu.js
@@ -1,42 +1,52 @@
 /**
-* ossi mainmenu class
-*/
-ossi.mainmenu = Class.create(ossi.base,{
-	initialize: function(parent,options) {
+ * ossi mainmenu class
+ */
+ossi.mainmenu = Class.create(ossi.base, {
+  initialize: function(parent, options){
     this.parent = parent;
-		this.options = Object.extend({
-      hostElement : false,
-      backCase : function() { return false; }
-	  },options);
-	  this.pane = false;
+    this.options = Object.extend({
+      hostElement: false,
+      backCase: function(){
+        return false;
+      }
+    }, options);
+    this.pane = false;
     this._draw();
-	},
-	/**
-	* _update
-	*
-	* does not handle XHR failure yet!
-	*/
-	update: function() {
-    if (typeof(this.parent.userId) == 'undefined') return; // userId in the parent controller not set
+  },
+  /**
+   * _update
+   *
+   * does not handle XHR failure yet!
+   */
+  update: function(){
+    if (typeof(this.parent.userId) == 'undefined') 
+      return; // userId in the parent controller not set
     var self = this;
-    var URL = BASE_URL+'/people/@me/@self';
+    var URL = BASE_URL + '/people/@me/@self';
     self.parent.showLoading();
     new Ajax.Request(URL, {
-      method : 'get',
-      requestHeaders : (client.is_widget && self.parent.sessionCookie) ? ['Cookie',self.parent.sessionCookie] : '',
-      onSuccess : function(response) {
+      method: 'get',
+      requestHeaders: (client.is_widget && self.parent.sessionCookie) ? ['Cookie', self.parent.sessionCookie] : '',
+      onSuccess: function(response){
         var json = response.responseJSON;
-								json = json.entry;
+        json = json.entry;
         var name = (json.name != null) ? json.name['unstructured'] : json.username; // if name has not been set
         self.parent.userName = name;
         $('mainmenu_profile_name').update(name);
         var status_message = false;
         var status_time = false;
-
-        if (typeof(json.role)  != 'undefined' && json.role != null){
+        var location = '';
+        if (typeof(json.location) != 'undefined') {
+          if (typeof(json.location.label) != 'undefined') {
+            if (json.location.label != null) {
+              location = ' @ ' + json.location.label;
+            }
+          }
+        }
+        if (typeof(json.role) != 'undefined' && json.role != null) {
           self.parent.userRole = json.role;
         }
-
+        
         if (typeof(json.status) != 'undefined') {
           if (json.status.message != 'undefined') {
             if (json.status.message != null) {
@@ -49,53 +59,46 @@ ossi.mainmenu = Class.create(ossi.base,{
             }
           }
         }
-        if (status_message) $('mainmenu_status_text').update(status_message);
-        URL = BASE_URL+'/people/@me/@pending_friend_requests'
-        new Ajax.Request(URL,{
-          method : 'get',
-          requestHeaders : (client.is_widget && self.parent.sessionCookie) ? ['Cookie',self.parent.sessionCookie] : '',
-          onSuccess : function(response) {
+        if (status_message) {
+          $('mainmenu_status_text').update(status_message + location);
+        }
+        URL = BASE_URL + '/people/@me/@pending_friend_requests'
+        new Ajax.Request(URL, {
+          method: 'get',
+          requestHeaders: (client.is_widget && self.parent.sessionCookie) ? ['Cookie', self.parent.sessionCookie] : '',
+          onSuccess: function(response){
             var json = response.responseJSON;
-            if (json.entry.length > 0) Element.insert($('friends_button'),' <span style="font-size:10px; font-weight:bold;">('+json.entry.length+' new)</span>');
-
-              // get location
-              URL = BASE_URL+'/people/@me/@location';
-              new Ajax.Request(URL,{
-                method : 'get',
-                requestHeaders : (client.is_widget && self.parent.sessionCookie) ? ['Cookie',self.parent.sessionCookie] : '',
-                onSuccess : function(response) {
-                  var json = response.responseJSON;
-  								json = json.entry;
-                  if (!Object.isUndefined(json.label) && json.label.length > 3) {
-                    $('mainmenu_profile_name').insert(' @ ' + json.label + ' ' + self.parent.utils.agoString(json.updated_at));
-                  } else if (Object.isNumber(json.latitude) && Object.isNumber(json.longitude)) {
-                    $('mainmenu_profile_name').insert(' @ ' + self.parent.utils.roundNumber(json.latitude,4) + ' / ' + self.parent.utils.roundNumber(json.longitude,4) + ' ' + self.parent.utils.agoString(json.updated_at));
-                  }
-                }
-              });
-              setTimeout(function() {
-                self.parent.hideLoading();
-              }, 600);
-            }
+            if (json.entry.length > 0) 
+              Element.insert($('friends_button'), ' <span style="font-size:10px; font-weight:bold;">(' + json.entry.length + ' new)</span>');
+            
+            setTimeout(function(){
+              self.parent.hideLoading();
+            }, 600);
+          }
         });
       }
     });
-	},
-  _draw: function() {
+  },
+  _draw: function(){
     if (this.options.hostElement) {
       this.options.hostElement.insert(this._getHTML());
       this._addListeners();
       this.pane = $('mainmenupane');
-    } else {
+    }
+    else {
       alert('ossi.mainmenu._draw() failed! this.options.hostElement not defined!');
     }
   },
-  _getHTML: function() {
-    var h =   '\
-          			<div id="mainmenupane" style="display:none; position:absolute; top:0px; left:0px; width:'+client.dimensions.width+'px">\
+  _getHTML: function(){
+    var h = '\
+          			<div id="mainmenupane" style="display:none; position:absolute; top:0px; left:0px; width:' + client.dimensions.width + 'px">\
           				<div id="logo_small"></div>\
           				<div id="microblog_button" class="post_button">\
-                    <div class="post_button_left_column"><img style="margin:2px 0px 0px 2px; border:solid #eee 1px;" src="'+BASE_URL+'/people/'+this.parent.userId+'/@avatar/small_thumbnail?'+Math.random()*9999+'" width="50" height="50" border="0" /></div>\
+                    <div class="post_button_left_column"><img style="margin:2px 0px 0px 2px; border:solid #eee 1px;" src="' +
+    BASE_URL +
+    '/people/@me/@avatar/small_thumbnail?' +
+    Math.random() * 9999 +
+    '" width="50" height="50" border="0" /></div>\
                     <div class="post_button_text">\
         						  <div class="button_title"><a id="mainmenu_profile_name" href="javascript:void(null);">n/a</a></div>\
         						  <div id="mainmenu_status_text" class="button_title" style="font-size:10px;">n/a</div>\
@@ -124,88 +127,103 @@ ossi.mainmenu = Class.create(ossi.base,{
           		';
     return h;
   },
-  _logoutHandler: function() {
+  _logoutHandler: function(){
     var self = this;
     this.parent.loadingpane.show();
-    new Ajax.Request(BASE_URL+'/session', {
-      method : 'delete',
-      requestHeaders : (client.is_widget) ? ['Cookie',self.parent.sessionCookie] : '',
-      onSuccess : function() {
+    new Ajax.Request(BASE_URL + '/session', {
+      method: 'delete',
+      requestHeaders: (client.is_widget) ? ['Cookie', self.parent.sessionCookie] : '',
+      onSuccess: function(){
         self.parent.sessionCookie = false;
-
+        
         delete self.parent.userId; //deleted so that attribute could be indicator of valid session.
-        delete self.parent.userName; 
+        delete self.parent.userName;
         delete self.parent.userRole;
-
+        
         self.parent.loadingpane.hide();
-  	    self.parent.case1({out : true});
+        self.parent.case1({
+          out: true
+        });
       },
-      onFailure : function() {
+      onFailure: function(){
         self.parent.loadingpane.hide();
         self.parent.case6({
-          backCase : self.parent.case3.bind(self.parent,{out:true}),
-          message : "Could not log user out.",
-          buttonText : "Try again"
+          message: "Could not log user out.",
+          buttonText: "Try again"
         });
       }
     });
   },
-  _channelsHandler: function() {
-    this.parent.case18({ backCase : this.parent.case3.bind(this.parent,{out:true}) });
+  _channelsHandler: function(){
+    this.parent.case18({});
   },
-  _groupsHandler: function() {
-    this.parent.case25({ backCase : this.parent.case3.bind(this.parent,{out:true}) });
+  _groupsHandler: function(){
+    this.parent.case25({});
   },
-  _friendsHandler: function() {
-    this.parent.case9({ backCase : this.parent.case3.bind(this.parent,{out:true}) });
+  _friendsHandler: function(){
+    this.parent.case9({});
   },
-  _feedsHandler: function() {
+  _feedsHandler: function(){
   },
-  _microblogHandler: function() {
-    this.parent.case7({ backCase : this.parent.case3.bind(this.parent,{out:true}) });
+  _microblogHandler: function(){
+    this.parent.case7({});
   },
-  _searchHandler: function() {
-    this.parent.case30({ backCase : this.parent.case3.bind(this.parent,{out:true}) });
+  _searchHandler: function(){
+    this.parent.case30({});
   },
-  _addListeners: function() {
-//    Element.observe does not work in Nokia Minimap browser when tabbed navigation is enabled,
-//    thus old skool
-
+  _addListeners: function(){
+    //    Element.observe does not work in Nokia Minimap browser when tabbed navigation is enabled,
+    //    thus old skool
+    
     $('logout_button').onclick = this._logoutHandler.bindAsEventListener(this);
     $('channels_button').onclick = this._channelsHandler.bindAsEventListener(this);
     $('groups_button').onclick = this._groupsHandler.bindAsEventListener(this);
     $('friends_button').onclick = this._friendsHandler.bindAsEventListener(this);
     $('feeds_button').onclick = this._feedsHandler.bindAsEventListener(this);
     $('microblog_button').onclick = this._microblogHandler.bindAsEventListener(this);
-	  $('search_button').onclick = this._searchHandler.bindAsEventListener(this);
-	
-/*    $('logout_button').observe('click',this._logoutHandler.bindAsEventListener(this));
-    $('channels_button').observe('click',this._channelsHandler.bindAsEventListener(this));
-    $('friends_button').observe('click',this._friendsHandler.bindAsEventListener(this));
-    $('feeds_button').observe('click',this._feedsHandler.bindAsEventListener(this));
-    $('microblog_button').observe('click',this._microblogHandler.bindAsEventListener(this));
-*/
+    $('search_button').onclick = this._searchHandler.bindAsEventListener(this);
+    
+    /*    $('logout_button').observe('click',this._logoutHandler.bindAsEventListener(this));
+     $('channels_button').observe('click',this._channelsHandler.bindAsEventListener(this));
+     $('friends_button').observe('click',this._friendsHandler.bindAsEventListener(this));
+     $('feeds_button').observe('click',this._feedsHandler.bindAsEventListener(this));
+     $('microblog_button').observe('click',this._microblogHandler.bindAsEventListener(this));
+     */
   },
-  _removeListeners: function() {
-//    Element.observe does not work in Nokia Minimap browser when tabbed navigation is enabled,
-//    thus old skool
-
-    $('logout_button').onclick = function() { return }
-    $('channels_button').onclick = function() { return }
-    $('groups_button').onclick = function() { return }
-    $('friends_button').onclick = function() { return }
-    $('feeds_button').onclick = function() { return }
-    $('microblog_button').onclick = function() { return }
-	  $('search_button').onclick = function() { return }
-
-/*    $('logout_button').stopObserving('click',this._logoutHandler.bindAsEventListener(this));
-    $('channels_button').stopObserving('click',this._channelsHandler.bindAsEventListener(this));
-    $('friends_button').stopObserving('click',this._friendsHandler.bindAsEventListener(this));
-    $('feeds_button').stopObserving('click',this._feedsHandler.bindAsEventListener(this));
-    $('microblog_button').stopObserving('click',this._microblogHandler.bindAsEventListener(this));
-*/
+  _removeListeners: function(){
+    //    Element.observe does not work in Nokia Minimap browser when tabbed navigation is enabled,
+    //    thus old skool
+    
+    $('logout_button').onclick = function(){
+      return
+    }
+    $('channels_button').onclick = function(){
+      return
+    }
+    $('groups_button').onclick = function(){
+      return
+    }
+    $('friends_button').onclick = function(){
+      return
+    }
+    $('feeds_button').onclick = function(){
+      return
+    }
+    $('microblog_button').onclick = function(){
+      return
+    }
+    $('search_button').onclick = function(){
+      return
+    }
+    
+    /*    $('logout_button').stopObserving('click',this._logoutHandler.bindAsEventListener(this));
+     $('channels_button').stopObserving('click',this._channelsHandler.bindAsEventListener(this));
+     $('friends_button').stopObserving('click',this._friendsHandler.bindAsEventListener(this));
+     $('feeds_button').stopObserving('click',this._feedsHandler.bindAsEventListener(this));
+     $('microblog_button').stopObserving('click',this._microblogHandler.bindAsEventListener(this));
+     */
   },
-  destroy: function () {
+  destroy: function(){
     this._removeListeners();
     this.pane.remove();
   }
