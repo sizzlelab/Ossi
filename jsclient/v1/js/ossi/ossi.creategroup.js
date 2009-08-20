aa57dcd77e0e958f5114757c964462e188ecd5d8
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-08-20 13:16:42 +0000

    Similar to channel creation.

--- jsclient/v1/js/ossi/ossi.creategroup.js
+++ jsclient/v1/js/ossi/ossi.creategroup.js
@@ -1,118 +1,110 @@
 /**
-* ossi creategroup class
-*/
-ossi.creategroup = Class.create(ossi.base,{
-	initialize: function(parent,options) {
+ * ossi creategroup class
+ */
+ossi.creategroup = Class.create(ossi.base, {
+  initialize: function(parent, options){
     this.parent = parent;
-		this.options = Object.extend({
-      hostElement : false
-	  },options);
-	  this.pane = false;
+    this.options = Object.extend({
+      hostElement: false
+    }, options);
+    this.pane = false;
     this._draw();
-	},
-  _draw: function() {
+  },
+  _draw: function(){
     if (this.options.hostElement) {
       this.options.hostElement.insert(this._getHTML());
       this._addListeners();
       this.pane = $('creategrouppane');
-      setTimeout(function() { $('create_group_form').focusFirstElement() },500); // .delay() did not seem to work on Firefox
-    } else {
+      setTimeout(function(){
+        $('create_group_form').focusFirstElement()
+      }, 500); // .delay() did not seem to work on Firefox
+    }
+    else {
       alert('ossi.creategroup._draw() failed! this.options.hostElement not defined!');
     }
   },
-  _getHTML: function() {
-    var h =   '\
+  _getHTML: function(){
+    var h = '\
           			<div id="creategrouppane" style="display:none; position:absolute; top:0px; left:0px; width:100%">\
                   <form name="create_group_form" id="create_group_form">\
                     <div style="margin: 12px auto 12px auto; text-align: left; width: 205px;">\
                       <dl>\
                         <dt style="color:#666; margin:0px 0px 5px 0px;">Group title:</dt>\
                           <dd style=" margin:0px 0px 10px 15px;"><input class="textinput" maxlength="30" name="group_title" id="group_title" type="text"/></dd>\
-                        <dt style="color:#666; margin:0px 0px 5px 0px;">Group type:</dt>\
-                          <dd style=" margin:0px 0px 10px 15px;">\
-                            <select name="group_type" id="group_type" style="color:#000;">\
-                              <option value="open">open</option>\
-                              <!--option value="closed">closed</option>\
-                              <option value="hidden">hidden</option-->\
-                              <!--option value="personal">personal</option-->\
-                            </select>\
-                          </dd>\
-                        <dt style="color:#666; margin:0px 0px 5px 0px;">Description:</dt>\
-                          <dd style=" margin:0px 0px 10px 15px;"><textarea class="textinput" maxlength="30" name="group_description" id="group_description" style="height:100px;" type="text"></textarea></dd>\
+                        <dt style="color:#666; margin:0px 0px 5px 0px;">Group description:</dt>\
+                          <dd style=" margin:0px 0px 10px 15px;"><input class="textinput" maxlength="30" name="group_description" id="group_description" type="text" /></dd>\
                       </dl>\
             				</div>\
             				<div style="height:14px"></div>\
             				<div class="nav_button">\
-            					<a id="create_group_create_button" class="nav_button_text" href="javascript:void(null);">Create</a>\
+            					<a id="create_open_group_create_button" class="nav_button_text" href="javascript:void(null);">Create</a>\
             				</div>\
-            				<div class="nav_button">\
-            					<a id="create_group_back_button" class="nav_button_text" href="javascript:void(null);">Back</a>\
-            				</div>\
-                  </form>\
-          			</div>\
-          		';
+            		  <div class="nav_button">\
+            		    <a id="create_group_back_button" class="nav_button_text" href="javascript:void(null);">Back</a>\
+            		   </div>\
+                 </form>\
+          			 </div>\
+              ';
     return h;
   },
-  _backHandler: function() {
+  
+  _backHandler: function(){
     this.options.backCase.apply();
   },
-  _createHandler: function() {
+  
+  _createHandler: function(){
     var self = this;
-    if (typeof(this.parent.userId) == 'undefined') return; // userId in the parent controller not set
-    var params = {  
-      'group[title]' : $F('group_title'),
-      'group[type]' : $F('group_type'),
-      'group[description]' : $F('group_description'),
-      'create_channel' : 'true'
+    var type = arguments[1];
+    if (typeof(this.parent.userId) == 'undefined') 
+      return; // userId in the parent controller not set
+    var params = {
+      'group[title]': $F('group_title'),
+      'group[type]': type,
+      'group[description]': $F('group_description'),
+      'create_channel': 'true'
     };
-
-    var URL = BASE_URL+'/groups/'; // ossi app id hard coded
+    
+    var URL = BASE_URL + '/groups/'; // ossi app id hard coded
     self.parent.showLoading();
-    new Ajax.Request(URL,{
-      method : 'post',
-      requestHeaders : (client.is_widget) ? ['Cookie',self.parent.sessionCookie] : '',
-      parameters : params,
-      onSuccess : function(response) { // now post the new group's collection ID and title to group list collection
+    new Ajax.Request(URL, {
+      method: 'post',
+      requestHeaders: (client.is_widget) ? ['Cookie', self.parent.sessionCookie] : '',
+      parameters: params,
+      onSuccess: function(response){ // now post the new group's collection ID and title to group list collection
         self.parent.hideLoading();
+        self.parent.stack.pop();
         self.parent.case6({
-          message : "Group created!",
-          buttonText : "Back to group list",
-          backCase : self.parent.case25.bind(self.parent,{
-            out:true,
-            backCase:self.parent.case3.bind(self.parent,{ out:true })
-          })
+          message: "Group created!",
+          buttonText: "Back to group list",
         });
       },
-      onFailure : function(response) {
+      onFailure: function(response){
         var json = response.responseJSON;
         var reason_string = '';
-        json.messages.each( function(error){
+        json.messages.each(function(error){
           reason_string += error + " ";
-        }, self );
+        }, self);
         self.parent.hideLoading();
         self.parent.case6({
-          message : "Could not create group: "+reason_string+"<br /><br>If this problem persist, please please post a comment on Ossi's \"Features & Bugs\" channel, or email us at <a href=\"mailto:otasizzle-helpdesk@hiit.fi\">otasizzle-helpdesk@hiit.fi</a>.</p>",
-          buttonText : "Try again",
-          backCase : self.parent.case26.bind(self.parent,{
-            out:true,
-            backCase:self.parent.case25.bind(self.parent,{
-              out:true,
-              backCase:self.parent.case3.bind(self.parent,{out:true})
-            })
-          })
+          message: "Could not create group: " + reason_string + "<br /><br>If this problem persist, please please post a comment on Ossi's \"Features & Bugs\" channel, or email us at <a href=\"mailto:otasizzle-helpdesk@hiit.fi\">otasizzle-helpdesk@hiit.fi</a>.</p>",
+          buttonText: "Try again",
         });
       }
     });
   },
-  _addListeners: function() {
-    $('create_group_create_button').onclick = this._createHandler.bindAsEventListener(this);
+  _addListeners: function(){
+    $('create_open_group_create_button').onclick = this._createHandler.bindAsEventListener(this, 'open');
     $('create_group_back_button').onclick = this._backHandler.bindAsEventListener(this);
   },
-  _removeListeners: function() {
-    $('create_group_create_button').onclick = function() { return };
-    $('create_group_back_button').onclick = function() { return };
+  _removeListeners: function(){
+    $('create_open_group_create_button').onclick = function(){
+      return
+    };
+    $('create_group_back_button').onclick = function(){
+      return
+    };
   },
-  destroy: function () {
+  destroy: function(){
     this._removeListeners();
     this.pane.remove();
   }
