86586497e68ebbba39bcfbf150e5aca02ef13e98
mnelimar <mnelimar@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-08-28 11:51:33 +0000

    Added automatical going back from uploading

--- jsclient/v1/js/ossi/ossi.avatar.js
+++ jsclient/v1/js/ossi/ossi.avatar.js
@@ -1,46 +1,42 @@
 /**
-* ossi avatar class
-*/
-ossi.avatar = Class.create(ossi.base,{
-	initialize: function(parent,options) {
+ * ossi avatar class
+ */
+ossi.avatar = Class.create(ossi.base, {
+  initialize: function(parent, options){
     this.parent = parent;
-		this.options = Object.extend({
-      hostElement : false,
-      backCase : function() { return false; }
-	  },options);
-	  this.pane = false;
-    this._draw();
-	},
-	/**
-	* _update
-	*
-	* does not handle XHR failure yet!
-	*/
-	update: function() {
-    if (typeof(this.parent.userId) == 'undefined') return; // userId in the parent controller not set
-    var self = this;
-    var URL = BASE_URL+'/people/'+this.parent.userId+'/@self';
-    self.parent.showLoading();
-    new Ajax.Request(URL, {
-      method : 'get',
-      requestHeaders : (client.is_Dashboard_widget && self.parent.sessionCookie) ? ['Cookie',self.parent.sessionCookie] : '',
-      onSuccess : function(response) {
+    this.options = Object.extend({
+      hostElement: false,
+      backCase: function(){
+        return false;
       }
-    });
-	},
-  _draw: function() {
+    }, options);
+    this.pane = false;
+    this._draw();
+  },
+  /**
+   * _update
+   *
+   * does not handle XHR failure yet!
+   */
+  update: function(){
+    if (typeof(this.parent.userId) == 'undefined') 
+      return; // userId in the parent controller not set
+    var self = this;
+  },
+  _draw: function(){
     if (this.options.hostElement) {
       this.options.hostElement.insert(this._getHTML());
       this._addListeners();
       this.pane = $('avatarpane');
-    } else {
+    }
+    else {
       alert('ossi.avatar._draw() failed! this.options.hostElement not defined!');
     }
   },
-  _getHTML: function() {
-    var h =   '\
+  _getHTML: function(){
+    var h = '\
           			<div id="avatarpane" style="display:none; position:absolute; top:0px; left:0px; width:100%">\
-                  <form target="avatar_frame" action="/cos/people/'+this.parent.userId+'/@avatar" method="post" enctype="multipart/form-data" name="avatar_form" id="avatar_form">\
+                  <form target="avatar_frame" action="/cos/people/@me/@avatar" method="post" enctype="multipart/form-data" name="avatar_form" id="avatar_form">\
                     <input name="_method" type="hidden" class="upload_pic" value="PUT" />\
             				<div id="about_Ossi" style="text-align:center; margin:12px 10px 12px 10px">\
                       <p>Please note that avatar upload does not work in iPhone due to platform restrictions, also on Ossi widget file upload is disabled by the platform itself. On N95 it will open a new window, which you can then close after upload to return to Ossi.</p>\
@@ -62,39 +58,43 @@ ossi.avatar = Class.create(ossi.base,{
           		';
     return h;
   },
-  _backHandler: function() {
+  _backHandler: function(){
     this.options.backCase.apply();
   },
-  _uploadHandler: function() {
+  _uploadHandler: function(){
     this.parent.showLoading();
-    $('avatar_form').submit();
-  },
-  _addListeners: function() {
-    $('upload_button').onclick = this._uploadHandler.bindAsEventListener(this);
-    $('back_button').onclick = this._backHandler.bindAsEventListener(this);
-
-    // put listener in global namespace for iframe upload
-    handleResponse = function() {
+    if ($F('avatar_file').length > 0) {
+      $('avatar_form').submit();
+      setTimeout(handleResponse, 7500);
+    }
+    else {
       this.parent.hideLoading();
       this.parent.case6({
-        message : "Avatar uploaded!",
-        buttonText : "Back to profile view",
-        backCase : this.parent.case8.bind(this.parent,{
-          out:true,
-          backCase: this.parent.case7.bind(this.parent,{
-            out:true,
-            backCase:this.parent.case3.bind(this.parent,{out:true})
-          })
-        })
+        message: "You need to select a file to be uploaded as an avantar."
       });
-    }.bindAsEventListener(this);
+    }
   },
-  _removeListeners: function() {
-    $('upload_button').onclick = function() { return };
-    $('back_button').onclick = function() { return };
+  _addListeners: function(){
+    $('upload_button').onclick = this._uploadHandler.bindAsEventListener(this);
+    $('back_button').onclick = this._backHandler.bindAsEventListener(this);
+    
+    // put listener in global namespace for iframe upload
+    handleResponse = function(){
+      this.parent.hideLoading();
+      this.options.backCase.apply();
+    }
+.bindAsEventListener(this);
+  },
+  _removeListeners: function(){
+    $('upload_button').onclick = function(){
+      return
+    };
+    $('back_button').onclick = function(){
+      return
+    };
     delete handleResponse; // remove listener from global namespace
   },
-  destroy: function () {
+  destroy: function(){
     this._removeListeners();
     this.pane.remove();
   }
