533079385de45b9689dbf2f9c8981023bc67c47a
turunen <turunen@6bfce0d2-394e-47ba-9216-cb80c28d6cb1>
2009-09-04 09:16:18 +0000

    fixed avatar upload

--- jsclient/v1/js/ossi/ossi.avatar.js
+++ jsclient/v1/js/ossi/ossi.avatar.js
@@ -36,8 +36,7 @@ ossi.avatar = Class.create(ossi.base, {
   _getHTML: function(){
     var h = '\
           			<div id="avatarpane" style="display:none; position:absolute; top:0px; left:0px; width:100%">\
-                  <form target="avatar_frame" action="/cos/people/@me/@avatar" method="post" enctype="multipart/form-data" name="avatar_form" id="avatar_form">\
-                    <input name="_method" type="hidden" class="upload_pic" value="PUT" />\
+                  <form target="avatar_frame" action="/cos/people/'+this.parent.userId+'/@avatar" method="post" enctype="multipart/form-data" name="avatar_form" id="avatar_form">\
             				<div id="about_Ossi" style="text-align:center; margin:12px 10px 12px 10px">\
                       <p>Please note that avatar upload does not work in iPhone due to platform restrictions, also on Ossi widget file upload is disabled by the platform itself. On N95 it will open a new window, which you can then close after upload to return to Ossi.</p>\
                     </div>\
@@ -66,8 +65,7 @@ ossi.avatar = Class.create(ossi.base, {
     if ($F('avatar_file').length > 0) {
       $('avatar_form').submit();
       setTimeout(handleResponse, 7500);
-    }
-    else {
+    } else {
       this.parent.hideLoading();
       this.parent.case6({
         message: "You need to select a file to be uploaded as an avantar."
@@ -82,16 +80,11 @@ ossi.avatar = Class.create(ossi.base, {
     handleResponse = function(){
       this.parent.hideLoading();
       this.options.backCase.apply();
-    }
-.bindAsEventListener(this);
+    }.bindAsEventListener(this);
   },
   _removeListeners: function(){
-    $('upload_button').onclick = function(){
-      return
-    };
-    $('back_button').onclick = function(){
-      return
-    };
+    $('upload_button').onclick = function() { return };
+    $('back_button').onclick = function() { return };
     delete handleResponse; // remove listener from global namespace
   },
   destroy: function(){
